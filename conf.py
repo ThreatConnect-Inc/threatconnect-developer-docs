05b89eb5e8fb05d2c71dd6da56ecef0387624109
slabe-svetlo <gsopko@threatconnect.com>
2017-02-10 16:30:11 -0500

    default conf.py

---
+++ conf.py
@@ -0,0 +1,72 @@
+# -*- coding: utf-8 -*-
+#
+import os
+import sys
+
+from recommonmark.parser import CommonMarkParser
+
+sys.path.insert(0, os.path.abspath('..'))
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "readthedocs.settings.dev")
+
+from django.conf import settings
+
+import django
+django.setup()
+
+
+sys.path.append(os.path.abspath('_ext'))
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.intersphinx',
+    'sphinxcontrib.httpdomain',
+    'djangodocs',
+]
+templates_path = ['_templates']
+
+source_suffix = ['.rst', '.md']
+source_parsers = {
+    '.md': CommonMarkParser,
+}
+
+master_doc = 'index'
+project = u'Read The Docs'
+copyright = u'2010, Eric Holscher, Charlie Leifer, Bobby Grace'
+version = '1.0'
+release = '1.0'
+exclude_patterns = ['_build']
+default_role = 'obj'
+pygments_style = 'sphinx'
+intersphinx_mapping = {
+    'python': ('http://python.readthedocs.io/en/latest/', None),
+    'django': ('http://django.readthedocs.io/en/1.8.x/', None),
+    'sphinx': ('http://sphinx.readthedocs.io/en/latest/', None),
+}
+# This doesn't exist since we aren't shipping any static files ourselves.
+#html_static_path = ['_static']
+htmlhelp_basename = 'ReadTheDocsdoc'
+latex_documents = [
+    ('index', 'ReadTheDocs.tex', u'Read The Docs Documentation',
+     u'Eric Holscher, Charlie Leifer, Bobby Grace', 'manual'),
+]
+man_pages = [
+    ('index', 'read-the-docs', u'Read The Docs Documentation',
+     [u'Eric Holscher, Charlie Leifer, Bobby Grace'], 1)
+]
+
+exclude_patterns = [
+    # 'api' # needed for ``make gettext`` to not die.
+]
+
+language = 'en'
+
+locale_dirs = [
+    'locale/',
+]
+gettext_compact = False
+
+
+on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
+if not on_rtd:  # only import and set the theme if we're building docs locally
+    import sphinx_rtd_theme
+    html_theme = 'sphinx_rtd_theme'
+    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
