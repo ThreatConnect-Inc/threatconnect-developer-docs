9fdbd2bc48011f58d89c1dd6ef7b77b686beab0d
Floyd Hightower <floyd.hightower27@gmail.com>
2017-04-20 08:26:22 -0400

    Adding missing comma

--- tests/test_links.py
+++ tests/test_links.py
@@ -21,7 +21,7 @@ def test_links():
     # TODO: consider dynamically pulling the links below:
     docs_pages = ['https://docs.threatconnect.com/en/latest/getting_started.html',
                   'https://docs.threatconnect.com/en/latest/rest_api/rest_api_docs.html',
-                  'https://docs.threatconnect.com/en/latest/python/python_sdk.html'
+                  'https://docs.threatconnect.com/en/latest/python/python_sdk.html',
                   'https://docs.threatconnect.com/en/latest/java/java_sdk.html',
                   'https://docs.threatconnect.com/en/latest/javascript/javascript_sdk.html',
                   'https://docs.threatconnect.com/en/latest/deployment_config.html',
