4e52759b6ba907ee4ec3967e7a4dcd32d752351c
Floyd Hightower <floyd.hightower27@gmail.com>
2017-05-12 14:43:44 -0400

    Adding meta scripts that I'm using to modify/build the docs

---
+++ .meta/doc_mass_modifier.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""Make changes in bulk by regex... USE WITH CAUTION!!!"""
+
+import os
+import re
+
+# set the language of the docs through which you would like to iterate <python|javascript|java|resp_api>
+docs_lang = "python"
+
+# iterate through the docs
+for path, dirs, files in os.walk("../docs/".format(docs_lang)):
+    # iterate through the files
+    for file_ in files:
+        file_text = None
+        full_file_path = os.path.join(path, file_)
+
+        # read the file's content
+        with open(full_file_path, 'r') as f:
+            file_text = f.read()
+
+        # find something in the current file
+        pattern = "# instantiate (.*) object"
+        matches = re.findall(pattern, file_text)
+        expected_matches = 1
+
+        if not matches:  # if there are no matches, move on
+            pass
+        elif len(matches) == expected_matches:  # if the number of matches is expected, make appropriate changes
+            # replace the matched content with something else
+            file_text = re.sub(pattern, "# instantiate some {}".format(matches[0]), file_text)
+
+            # write the update content to the file
+            with open(full_file_path, 'w') as f:
+                f.write(file_text)
+        else:  # if the number of matches is unexpected, print a warning
+            print("{} matches found in {}".format(len(matches), full_file_path))
