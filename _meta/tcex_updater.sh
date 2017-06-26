8f2a78ec909fc8af285c11f5c4456266196a2b57
Floyd Hightower <floyd.hightower27@gmail.com>
2017-06-26 15:42:50 -0400

    Adding shell script to update the tcex docs automatically

---
+++ _meta/tcex_updater.sh
@@ -0,0 +1,27 @@
+# move to the tcex docs dir (uses the `docs` alias that `cd`'s into the root directory of the documentation)
+docs
+cd ThreatConnect_Developer_Docs/;
+
+# checkout dev
+git checkout dev;
+
+# update tcex
+make uptcex;
+
+# change the variable name of the tcex version used in the tcex docs
+sed -i.bak 's/|version|/|tcex_version|/g' ./docs/tcex/tcex.rst && rm ./docs/tcex/tcex.rst.bak
+
+# stage all changes (including deletions)
+git add -A;
+
+# define a datestamp
+DATE=$(date +"%B %d, %Y");
+
+# commit
+git commit -m "Auto-update TCEX docs: $DATE";
+
+# push
+git push;
+
+# send slack notification (uses the `slack` alias which sends the given message to a slack channel)
+slack "TCEX_documentation_updated";
